e3d2937215f12d502af3fff75e2b3baa501e1a8a
shivasagar4d <sagar4dk@gmail.com>
2017-08-29 22:28:29 -0500

    Add files via upload

--- log-processing/ui.R
+++ log-processing/ui.R
@@ -1,30 +1,29 @@
 library(shiny)
 library(shinydashboard)
 library(DT)
-ui <- shinyUI(
-      fluidPage(
-      titlePanel("Complaints BI Screen"),
-      div(style="display: inline-block;vertical-align:top; width: 200px;",selectInput("LineofBusiness", "Select LineofBusiness",Dataset1$LineofBusiness,selected = "Select LineofBusiness",selectize = TRUE,multiple = TRUE)),
+ui <- shinyUI(fluidPage(
+      titlePanel("Demo"),
+      
+      div(style="display: inline-block;vertical-align:top; width: 200px;",selectInput("State", "Select State",Dataset1$State,selected = "Select State",selectize = TRUE,multiple = TRUE)),
       div(style="display: inline-block;vertical-align:top; width: 50px;",HTML("<br>")),
-      div(style="display: inline-block;vertical-align:top; width: 200px;",selectInput("EntitlementLevel", "Select EntitlementLevel",Dataset1$EntitlementLevel,selected = "Select EntitlementLevel",selectize = TRUE,multiple = TRUE)),
+      div(style="display: inline-block;vertical-align:top; width: 200px;",selectInput("Country", "Select Country",Dataset1$Country,selected = "Select Country",selectize = TRUE,multiple = TRUE)),
       div(style="display: inline-block;vertical-align:top; width: 50px;",HTML("<br>")),
-      div(style="display: inline-block;vertical-align:top; width: 200px;",selectInput("ComplaintCaptureSource", "Select ComplaintSource",Dataset1$ComplaintCaptureSource,selected = "Select ComplaintSource",selectize = TRUE,multiple = TRUE)),
+      div(style="display: inline-block;vertical-align:top; width: 200px;",selectInput("Device", "Select Device",Dataset1$device,selected = "Select Device",selectize = TRUE,multiple = TRUE)),
       div(style="display: inline-block;vertical-align:top; width: 50px;",HTML("<br>")),
-      div(style="display: inline-block;vertical-align:top; width: 1100px;",selectInput("ClosedNotes","Select ClosedNotes",Dataset1$ClosedNotes1,selected = NULL,selectize = TRUE,multiple = TRUE)),
+      div(style="display: inline-block;vertical-align:top; width: 200px;",selectInput("Comment","Select Comments",Dataset1$comments1,selected = NULL,selectize = TRUE,multiple = TRUE)),
+      
+      
+      br(),
+      br(),
+      
       br(),
       br(), 
       div(style="display: inline-block;vertical-align:top; width: 300px;",verbatimTextOutput("Counter", placeholder = FALSE)),
       div(style="display: inline-block;vertical-align:top; width: 8px;",HTML("<br>")),
       actionButton("Click", "Click Me"),
-
-      sidebarPanel(radioButtons("filetype", "File type:",choices = c("csv", "tsv")),downloadButton('downloadData', 'Download')),
-######  downloadData       
       br(),
       br(),
       br(),
       
-     
       uiOutput("dt")
-    )
-    
-   )
+    ))
